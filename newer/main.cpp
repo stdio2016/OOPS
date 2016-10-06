ede0f6cb0fedf2f6039f7824e989f9b0c6043c99
stdio2016 <randomfrank8@gmail.com>
2016-10-06 10:06:42 +0800

    Add files via upload

--- newer/main.cpp
+++ newer/main.cpp
@@ -7,29 +7,109 @@
 using namespace std;
 
 class SyntaxError : public runtime_error{
-
+public:
+    SyntaxError(string aa): runtime_error(aa){}
 };
 
 class tokenizer{
     istream *f;
     stringstream ss;
     void skiplinecomment();
+    void skipstarcomment();
+    void skipspace();
 public:
     tokenizer(istream &in): f(&in), ss(){}
     int next();
     string val;
-    static const int word=0;
-    static const int eof=-1;
+    static const int Name=0;
+    static const int String=1;
+    static const int Op=2;
+    static const int Eof=-1;
+    static bool isNameChar(int ch);
+    static bool isNameStartChar(int ch);
 };
+
+bool tokenizer::isNameChar(int ch){
+    return (ch>='a'&&ch<='z') || (ch>='A'&&ch<='Z') || (ch>='0'&&ch<='9') || ch=='_' || ch=='$';
+}
+
+bool tokenizer::isNameStartChar(int ch){
+    return (ch>='a'&&ch<='z') || (ch>='A'&&ch<='Z') || ch=='_' || ch=='$';
+}
+
+bool isSpace(int ch){
+    return ch==' ' || ch=='\n' || ch=='\r' || ch=='\t';
+}
 
 int tokenizer::next(){
     val="";
-    int ch=f->get();
-    if(ch==EOF)
-        return eof;
     ss.clear();
-    skiplinecomment();
-    return word;
+    skipspace();
+    int ch=f->get();
+    if(isNameStartChar(ch))
+    {
+        do{
+            ss.put(ch);
+            ch=f->get();
+        }while(isNameChar(ch)) ;
+        f->unget();
+        ss>>val;
+        return Name;
+    }
+    else {
+        switch(ch){
+            case EOF: return Eof;
+            case '"':
+                return String;
+            case ':':
+            case '{':
+            case '}':
+            case '(':
+            case ')':
+            case ',':
+            case '=':
+            case '.':
+            case ';':
+                ss.put(ch);
+                ss>>val;
+                return Op;
+            default:
+                ss.put(ch);
+                ss>>val;
+                throw SyntaxError(string("Illegal character: ")+val);
+        }
+    }
+    return Eof;
+}
+
+void tokenizer::skipspace()
+{
+    int ch;
+    bool space=true;
+    do {
+        ch=f->get();
+        switch(ch){
+        case EOF:
+            return;
+        case '/':
+            ch=f->get();
+            if(ch=='/')
+                skiplinecomment();
+            else if(ch=='*')
+                skipstarcomment();
+            else
+                throw SyntaxError("Illegal character: /");
+            break;
+        case ' ':
+        case '\n':
+        case '\r':
+        case '\t':
+            break;
+        default:
+            space=false;
+        }
+    } while(space);
+    f->unget();
 }
 
 void tokenizer::skiplinecomment()
@@ -41,16 +121,19 @@ void tokenizer::skiplinecomment()
     f->unget();
 }
 
-bool isNameChar(int ch){
-    return (ch>='a'&&ch<='z') || (ch>='A'&&ch<='Z') || (ch>='0'&&ch<='9') || ch=='_' || ch=='$';
-}
-
-bool isNameStartChar(int ch){
-    return (ch>='a'&&ch<='z') || (ch>='A'&&ch<='Z') || ch=='_' || ch=='$';
-}
-
-bool isSpace(int ch){
-    return ch==' ' || ch=='\n' || ch=='\r' || ch=='\t';
+void tokenizer::skipstarcomment()
+{
+    int ch=f->get();
+    int state=0;
+    while(state==0 || ch!='/'){
+        if(ch=='*')
+            state=1;
+        else
+            state=0;
+        ch=f->get();
+        if(ch==EOF)
+            throw SyntaxError("unterminated comment");
+    }
 }
 
 int main()
@@ -62,9 +145,18 @@ int main()
     }
     tokenizer tt(f);
     int status;
-    while(tt.next()==tokenizer::word){
-        cout<<tt.val;
+    int result=0;
+    try{
+        status=tt.next();
+        while(status!=tokenizer::Eof){
+            cout<<tt.val<<endl;
+            status=tt.next();
+        }
+    }
+    catch(SyntaxError error){
+        cout<<error.what();
+        result=1;
     }
     f.close();
-    return 0;
+    return result;
 }
